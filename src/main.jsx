83c64966f1aad1d1c9523b58a9f0a6e758574e26
aly0na27 <alenakravchenko1910@gmail.com>
2024-07-12 14:11:47 +0300

    >84

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
-import './../style.css'
 
 const rootElement = document.getElementById('app');
 const root = ReactDOM.createRoot(rootElement);
