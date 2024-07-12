ca3335a71ff901b63216ee82b0a419ce6cd2248c
aly0na27 <alenakravchenko1910@gmail.com>
2024-07-12 14:01:51 +0300

    >84

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '',
-  server: {
-    port: 3000,
-  },
   optimizeDeps: {
     include: ['react', 'react-dom'],
+  },
+  css: {
+    minify: true, // Включить минификацию CSS
+  },
+  html: {
+    minify: true
+  },
+  build: {
+    assetsInlineLimit: 0, // Отключает встраивание изображений как data URI
   },
 })
